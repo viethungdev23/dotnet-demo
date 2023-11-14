c388918a4430ed8e3e9f63d5f2cff2333510a832
viethung <viethungdev23@gmail.com>
2023-11-14 11:54:06 +0000

    add new comment 2

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // new comment
+// new comment 2
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
