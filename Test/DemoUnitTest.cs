2cb68390411a5a07fdbd33e660833e69af8cdeb4
viethung <viethungdev23@gmail.com>
2023-11-14 10:00:57 +0000

    init

---
+++ Test/DemoUnitTest.cs
@@ -0,0 +1,28 @@
+namespace Test;
+
+public class DemoUnitTest
+{
+    [Fact]
+    public void TestAdd()
+    {
+        // Arrange
+        Calculator calculator = new Calculator();
+        int x = 3;
+        int y = 5;
+        int expectedSum = 8;
+
+        // Act
+        int actualSum = calculator.Add(x, y);
+
+        // Assert
+        Assert.Equal(expectedSum, actualSum);
+    }
+}
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b ;
+    }
+}
